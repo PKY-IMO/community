f0c232245fcfbdf90bb13883524a69e63b6062a9
pankeyu <1029641620@qq.com>
2021-03-04 17:38:48 +0800

    first commit

---
+++ src/main/java/com/pan/community/dao/UserMapper.java
@@ -0,0 +1,21 @@
+package com.pan.community.dao;
+
+import com.pan.community.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    User selectById(int id);
+
+    User selectByName(int username);
+
+    User selectByEmail(String email);
+
+    int insertUser(User user);
+
+    int updateStatus(int id, int status);
+
+    int updateHeader(int id, String headerUrl);
+
+    int updatePassword(int id, String password);
+}
---
+++ src/test/java/com/pan/community/MapperTest.java
@@ -0,0 +1,43 @@
+package com.pan.community;
+
+import com.pan.community.dao.DiscussPostMapper;
+import com.pan.community.dao.UserMapper;
+import com.pan.community.entity.DiscussPost;
+import com.pan.community.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Test
+    public void testSelectUser(){
+        User user = userMapper.selectById(101);
+        System.out.println(user);
+    }
+
+    @Test
+    public void selectPosts(){
+        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(149,0,10);
+        for (DiscussPost dis:list) {
+            System.out.println(dis);
+        }
+        int rows = discussPostMapper.selectDiscussPostRows(149);
+        System.out.println(rows);
+    }
+
+}
